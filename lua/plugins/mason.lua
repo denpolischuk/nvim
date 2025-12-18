cc63ec9db35dbfb22579a055c027fa6b67c97e42
Denys Polishchuk <denys.polishchuk@enpal.de>
2025-12-18 11:50:27 +0100

    feat: conform lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,18 +1,18 @@
 return {
-{
-  "williamboman/mason.nvim",
-  build = ":MasonUpdate",
-  config = function()
-    require("mason").setup()
-  end,
-},
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-{
-  "williamboman/mason-lspconfig.nvim",
-  config = function()
-    require("mason-lspconfig").setup {
-      ensure_installed = { "lua_ls", "gopls", "terraformls", "pylsp", "helm_ls" },
-    }
-  end,
-},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "gopls", "terraformls", "pylsp", "helm_ls" },
+			})
+		end,
+	},
 }
