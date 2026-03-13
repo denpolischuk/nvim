00a0c56ba1846c0a7668d7cabc889693728122b7
Denys Polishchuk <denys.polishchuk@enpal.de>
2026-03-13 09:14:18 +0100

    feat: add clipboard copy

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -4,26 +4,32 @@ vim.g.maplocalleader = " "
 
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR><Esc>", { silent = true })
-vim.keymap.set("n", "<Leader>h", "<CMD>LspStop<CR><BAR><CMD>set ft=helm<CR>", { silent = true, desc = "Switch file type to helm" })
+vim.keymap.set(
+	"n",
+	"<Leader>h",
+	"<CMD>LspStop<CR><BAR><CMD>set ft=helm<CR>",
+	{ silent = true, desc = "Switch file type to helm" }
+)
 
 -- tabs
-vim.keymap.set({ "n", }, "[t", ":tabprevious<CR>")
-vim.keymap.set({ "n", }, "]t", ":tabnext<CR>")
+vim.keymap.set({ "n" }, "[t", ":tabprevious<CR>")
+vim.keymap.set({ "n" }, "]t", ":tabnext<CR>")
 
 -- buffers
-vim.keymap.set({ "n", }, "H", ":bprevious<CR>")
-vim.keymap.set({ "n", }, "L", ":bnext<CR>")
+vim.keymap.set({ "n" }, "H", ":bprevious<CR>")
+vim.keymap.set({ "n" }, "L", ":bnext<CR>")
 
 -- windows
-vim.keymap.set({ "n", }, "|", ":vsplit<CR>")
-vim.keymap.set({ "n", }, "_", ":split<CR>")
-vim.keymap.set({ "n", }, "<C-j>", ":wincmd j<CR>")
-vim.keymap.set({ "n", }, "<C-k>", ":wincmd k<CR>")
-vim.keymap.set({ "n", }, "<C-h>", ":wincmd h<CR>")
-vim.keymap.set({ "n", }, "<C-l>", ":wincmd l<CR>")
+vim.keymap.set({ "n" }, "|", ":vsplit<CR>")
+vim.keymap.set({ "n" }, "_", ":split<CR>")
+vim.keymap.set({ "n" }, "<C-j>", ":wincmd j<CR>")
+vim.keymap.set({ "n" }, "<C-k>", ":wincmd k<CR>")
+vim.keymap.set({ "n" }, "<C-h>", ":wincmd h<CR>")
+vim.keymap.set({ "n" }, "<C-l>", ":wincmd l<CR>")
 
-vim.keymap.set({ "n", }, "<C-left>", ":wincmd <<CR>")
-vim.keymap.set({ "n", }, "<C-right>", ":wincmd ><CR>")
-vim.keymap.set({ "n", }, "<C-up>", ":wincmd +<CR>")
-vim.keymap.set({ "n", }, "<C-down>", ":wincmd -<CR>")
+vim.keymap.set({ "n" }, "<C-left>", ":wincmd <<CR>")
+vim.keymap.set({ "n" }, "<C-right>", ":wincmd ><CR>")
+vim.keymap.set({ "n" }, "<C-up>", ":wincmd +<CR>")
+vim.keymap.set({ "n" }, "<C-down>", ":wincmd -<CR>")
 
+vim.keymap.set({ "v" }, "Y", '"+y')
